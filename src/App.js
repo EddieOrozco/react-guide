2cd08b68d9c0195512511456739478e0ec0ab382
EddieOrozco <orozcoe230@gmail.com>
2020-01-31 09:56:36 -0600

    Using Methods to change state properties with setState

--- src/App.js
+++ src/App.js
@@ -13,11 +13,25 @@ class App extends Component {
       }
     ]
   }
+  switchNameHandler = () => {
+    // console.log("Clicked")
+    this.setState( {
+       persons: [
+        {
+         name: 'Edd', age: 21
+        },
+       {
+          name: 'Jo', age: 34
+        }
+      ]
+    } )
+  }
   render() {
     return (
       <div className="App">
         <h1>Yo! Whats up?</h1>
         <p>This is Working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Building pcs</Person>
       </div>
