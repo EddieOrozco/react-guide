dc2c2b7d2e64d0399aeb73ba679bd5622486e705
EddieOrozco <orozcoe230@gmail.com>
2020-02-10 10:43:25 -0600

    How to delete an item in a array with modern syntax and how to use the KEY to keep track of the item in the array and get rid of error message

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ class App extends Component {
   state = {
     persons: [
       {
+        id: "fsf",
         name: "Eddie",
         age: 28
       },
       {
+        id: "sff",
         name: "Josh",
         age: 30
       }
@@ -46,6 +48,13 @@ class App extends Component {
       ]
     });
   };
+
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
@@ -65,8 +74,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+              />
+            );
           })}
         </div>
       );
