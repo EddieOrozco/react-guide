cccd625e218da7765bc9e93baa247536e460fd22
EddieOrozco <orozcoe230@gmail.com>
2020-02-10 09:17:44 -0600

    Using the map() function to loop through an array and display each person

--- src/App.js
+++ src/App.js
@@ -1,94 +1,88 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person.js'
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person.js";
 
 class App extends Component {
   state = {
     persons: [
       {
-        name: 'Eddie', age: 28
+        name: "Eddie",
+        age: 28
       },
       {
-        name: 'Josh', age: 30
+        name: "Josh",
+        age: 30
       }
     ],
     showPersons: false
-  }
-  switchNameHandler = (newName) => {
+  };
+  switchNameHandler = newName => {
     // console.log("Clicked")
-    this.setState( {
-       persons: [
+    this.setState({
+      persons: [
         {
-         name: newName, age: 21
+          name: newName,
+          age: 21
         },
-       {
-          name: 'Jo', age: 34
+        {
+          name: "Jo",
+          age: 34
         }
       ]
-    } )
-  }
+    });
+  };
 
-  nameChangeHandler = (event) => {
-    this.setState( {
+  nameChangeHandler = event => {
+    this.setState({
       persons: [
-       {
-        name: 'Edd', age: 21
-       },
-      {
-         name: event.target.value, age: 34
-       }
-     ]
-   } )  
-  }
-  togglePersonsHandler= () => {
+        {
+          name: "Edd",
+          age: 21
+        },
+        {
+          name: event.target.value,
+          age: 34
+        }
+      ]
+    });
+  };
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor:'pointer'
-    }
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
 
     let persons = null;
 
-    if ( this.state.showPersons){
+    if (this.state.showPersons) {
       persons = (
-        <div >
-          <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this,'Edd!!!')}
-          />
-          <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          changed={this.nameChangeHandler}>
-          My hobbies: Building pcs
-          </Person>
-        </div> 
-      )
+        <div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+        </div>
+      );
     }
 
     return (
       <div className="App">
         <h1>Yo! Whats up?</h1>
         <p>This is Working</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>
+        <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
-          {persons}
+        {persons}
       </div>
     );
   }
 }
-
 
 export default App;
